27a0084ff95e5db47555f2781d22398594f90721
Geraldo Eloi <geraldo.eloi11@gmail.com>
2022-02-19 13:56:59 -0300

    Book model

---
+++ src/main/java/br/com/desenvolvedores/BookStore/model/Book.java
@@ -0,0 +1,16 @@
+package br.com.desenvolvedores.BookStore.model;
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@Document(collection = "books")
+public class Book {
+    @Id
+    private String id;
+    private String name;
+    private String description;
+    private String author;
+    private String genre;
+}
