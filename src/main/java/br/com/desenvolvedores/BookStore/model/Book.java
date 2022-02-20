544d6b9b261599453e6299dc366627d08a07c3c9
Geraldo Eloi <geraldo.eloi11@gmail.com>
2022-02-20 14:04:43 -0300

    Book model as Entity

--- src/main/java/br/com/desenvolvedores/BookStore/model/Book.java
+++ src/main/java/br/com/desenvolvedores/BookStore/model/Book.java
@@ -1,6 +1,6 @@
 package br.com.desenvolvedores.BookStore.model;
 
-import lombok.Data;
+import lombok.*;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -8,13 +8,26 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Data
+@NoArgsConstructor
 @Entity(name = "books")
 public class Book {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Setter(AccessLevel.NONE)
     private Long id;
     private String name;
     private String description;
     private String author;
     private String genre;
+
+    @Builder
+    private static Book bookFactory(String name, String description, String author, String genre) {
+        Book book = new Book();
+        book.setName(name);
+        book.setDescription(description);
+        book.setAuthor(author);
+        book.setGenre(genre);
+
+        return book;
+    }
 }
